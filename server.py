bf2fc4dac657cd860145d95615029af97906d11e
Max R <q121212@gmail.com>
2017-07-22 16:26:03 +0300

    was added signal handler

--- server.py
+++ server.py
@@ -4,25 +4,33 @@
 import socket
 import logger
 
-timeout = 600
-host = '192.168.0.52'
-port = 7777
-addr = (host, port)
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(addr)
+def start_server(host='192.168.0.52', port=7777):
+	timeout = 600
+	addr = (host, port)
+	server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	server.bind(addr)
 
-print ('Waiting for data ({0} seconds)...'.format(timeout))
-while True:
-	server.settimeout(timeout)
-	try:
-		d = server.recvfrom(1048576)
-	except socket.timeout: 
-		print('Time is out. {0} seconds have passed'.format(timeout))
-		break
-	received = d
-	addr = d[1]
-	print ('Received data: ' , received)
-	logger.insert_text(str(received[0]) + ' ' + str(received[1]))
-	msg = "The message was received.".format(received[0])
-	server.sendto(msg.encode('utf-8'), addr)
-server.close()
+	print ('Waiting for data ({0} seconds)...'.format(timeout))
+	while True:
+		server.settimeout(timeout)
+		try:
+			d = server.recvfrom(1048576)
+		except socket.timeout: 
+			print('Time is out. {0} seconds have passed'.format(timeout))
+			break
+		received = d
+		addr = d[1]
+		print ('Received data: ' , received)
+		logger.insert_text(str(received[0]) + ' ' + str(received[1]))
+		msg = "The message was received.".format(received[0])
+		server.sendto(msg.encode('utf-8'), addr)
+		handler(received[0])
+	server.close()
+
+def handler(message):
+	if message == b'Exit':
+		print("Bye!\nReceived signal to Exit.")
+		exit()
+
+if __name__ == '__main__':
+	start_server()
